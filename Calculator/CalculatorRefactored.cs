61ffc1e28dad8974d092cdf3a03c96559d9fc5f1
Laimonas_Ciurlionis <laimonas.ciurlionis@gmail.com>
2022-03-24 20:32:45 +0200

    calculator refactored

---
+++ Calculator/CalculatorRefactored.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Linq;
+
+namespace Calculator
+{
+    public class CalculatorRefactored
+    {
+        private static readonly string[] _validOperators = { "+", "-", "*", "/" };
+
+        static void MainCopy()
+        {
+            while (true)
+            {
+                try
+                {
+                    double firstNum = GetNumberFromConsole();
+                    double secondNum = GetNumberFromConsole();
+                    string operatorValue = GetOperatorFromConsole();
+
+                    double result = PerformCalculation(firstNum, secondNum, operatorValue);
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error occured: {ex.Message}");
+                }
+            }
+        }
+
+        public static double PerformCalculation(double firstNum, double secondNum, string operation)
+        {
+            return operation switch
+            {
+                "+" => Add(firstNum, secondNum),
+                "-" => Minus(firstNum, secondNum),
+                "*" => Multiply(firstNum, secondNum),
+                "/" => Divide(firstNum, secondNum),
+                _ => 0
+            };
+        }
+
+        private static double GetNumberFromConsole()
+        {
+            bool isCorrectNumber = false;
+            double argumentValue = 0;
+
+            while (!isCorrectNumber)
+            {
+                string userInputValue = GetUserInput(true);
+
+                CheckApplicationExit(userInputValue);
+
+                isCorrectNumber = double.TryParse(userInputValue, out argumentValue);
+
+                if (!isCorrectNumber) 
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
+
+            return argumentValue;
+        }
+
+        private static string GetOperatorFromConsole()
+        {
+            bool isCorrectOperator = false;
+            string operatorValue = string.Empty;
+
+            while (!isCorrectOperator)
+            {
+                string userInputValue = GetUserInput(false);
+
+                CheckApplicationExit(userInputValue);
+
+                isCorrectOperator = IsOperatorValid(userInputValue);
+                operatorValue = userInputValue;
+
+                if (!isCorrectOperator) 
+                {
+                    Console.WriteLine("Please enter a valid operator");
+                }
+            }
+
+            return operatorValue;
+        }
+
+        private static double Add(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        private static double Minus(double firstNum, double secondNum)
+        {
+            return firstNum - secondNum;
+        }
+
+        private static double Multiply(double firstNum, double secondNum)
+        {
+            return firstNum * secondNum;
+        }
+
+        private static double Divide(double firstNum, double secondNum)
+        {
+            if (secondNum == 0)
+            {
+                throw new DivideByZeroException("Can not devide by zero!!! Go learn some math.");
+            }
+
+            return firstNum / secondNum;
+        }
+
+        private static bool IsOperatorValid(string userInputValue)
+        {
+            return _validOperators.Contains(userInputValue);
+        }
+
+        private static void CheckApplicationExit(string input)
+        {
+            if (input.Equals("q", StringComparison.OrdinalIgnoreCase))
+            {
+                CloseApplication();
+            }
+        }
+
+        private static void CloseApplication()
+        {
+            Console.WriteLine("Goodbey, thank you for using our Calculator");
+            Console.WriteLine("Press any key to close application....");
+            Environment.Exit(0);
+        }
+
+        private static string GetUserInput(bool isNumber) 
+        {
+            if (isNumber)
+            {
+                Console.WriteLine("Enter a number or Q to exit");
+            }
+            else 
+            {
+                Console.WriteLine($"Enter an operator or Q to exit. Allowed operators: {string.Join(", ", _validOperators)}");
+            }
+
+            return Console.ReadLine();
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,7 +12,47 @@ namespace Calculator
 
         static void Main(string[] args)
         {
-          
+            while (true) 
+            {
+                try
+                {
+                    double firstNum = GetNumberFromConsole();
+                    double secondNum = GetNumberFromConsole();
+                    string operatorValue = GetOperatorFromConsole();
+
+                    double result = PerformCalculation(firstNum, secondNum, operatorValue);
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error occured: {ex.Message}");
+                }
+            }
+        }
+
+        public static double PerformCalculation(double firstNum, double secondNum, string operation) 
+        {
+            double result = 0;
+            switch (operation) 
+            {
+                case "+":
+                    result = Add(firstNum, secondNum);
+                    break;
+                case "-":
+                    result = Minus(firstNum, secondNum);
+                    break;
+                case "*":
+                    result = Multiply(firstNum, secondNum);
+                    break;
+                case "/":
+                    result = Divide(firstNum, secondNum);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
         }
 
         private static double GetNumberFromConsole() 
@@ -64,6 +104,33 @@ namespace Calculator
             }
 
             return operatorValue;
+        }
+
+        private static double Add(double firstNum, double secondNum) 
+        {
+            //double sum = firstNum + secondNum;
+            //return sum;
+            return firstNum + secondNum;
+        }
+
+        private static double Minus(double firstNum, double secondNum) 
+        {
+            return firstNum - secondNum;
+        }
+
+        private static double Multiply(double firstNum, double secondNum) 
+        {
+            return firstNum * secondNum;
+        }
+
+        private static double Divide(double firstNum, double secondNum) 
+        {
+            if (secondNum == 0) 
+            {
+                throw new DivideByZeroException("Can not devide by zero!!! Go learn some math.");
+            }
+
+            return firstNum / secondNum;
         }
 
         private static bool IsOperatorValid(string userInputValue) 

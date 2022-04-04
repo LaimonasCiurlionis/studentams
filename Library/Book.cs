c08b496fee34e365179b7bacf7c5295916743f12
Laimonas_Ciurlionis <laimonas.ciurlionis@gmail.com>
2022-04-04 22:06:15 +0300

    Get/Set ir Library

---
+++ Library/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public struct Book
+    {
+        private string _name;
+        private string _id;
+
+        public string Name //Properties
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                CreateLocalId();
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                CreateLocalId();
+            }
+        }
+
+        public string LocalId { get; private set; }
+
+        public Book(string name, string id)
+        {
+            _name = name;
+            _id = id;
+            LocalId = string.Empty;
+            CreateLocalId();
+        }
+
+        private void CreateLocalId() 
+        {
+            LocalId = string.Format("{0}_{1}", Name, Id);
+        }
+    }
+}
---
+++ Topic_8_Biblioteka/Program.cs
@@ -0,0 +1,19 @@
+﻿using Library;
+using System;
+
+namespace Topic_8_Biblioteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Super book", "123456");
+            book.Name = "Some Name";
+            book.Id = "789456";
+
+        
+            Console.WriteLine(book.Name);
+            Console.WriteLine(book.Id);
+        }
+    }
+}

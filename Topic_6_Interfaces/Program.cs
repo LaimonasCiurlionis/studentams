1f34f76e89bd09a4164cc831794a6e31049d5e4a
Laimonas_Ciurlionis <laimonas.ciurlionis@gmail.com>
2022-05-17 21:18:56 +0300

    task

--- Topic_6_Interfaces/Program.cs
+++ Topic_6_Interfaces/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Topic_6_Interfaces
 {
@@ -21,6 +22,60 @@ namespace Topic_6_Interfaces
             var bmwCar2 = new BmwCar("Model5", true, 5);
 
             bool isEqual = bmwCar.Equals(bmwCar2);
+
+            var animals = new List<Animal> { new Cat { Name = "Cat1" }, new Cat { Name = "Cat2" }, new Dog { Name = "Dog1" } };
+            animals.Sort();
+        }
+    }
+
+    public interface IAnimal 
+    {
+        void Eat();
+    }
+
+    public interface IMammal 
+    {
+        void GiveBirth();
+    }
+
+    public class Animal : IAnimal, IComparable<Animal>
+    {
+        public string Name { get; set; }
+
+        public int CompareTo(Animal other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+
+    public class Dog : Animal, IMammal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("DOG is eating. AU AU.");
+        }
+
+        public void GiveBirth()
+        {
+            Console.WriteLine("DOG is giving brith. AU AU.");
+        }
+    }
+
+    public class Cat : Animal, IMammal 
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("CAT is eating. MIAUW MIAUW.");
+        }
+
+        public void GiveBirth()
+        {
+            Console.WriteLine("CAT is giving brith. MIAUW MIAUW.");
         }
     }
 }
